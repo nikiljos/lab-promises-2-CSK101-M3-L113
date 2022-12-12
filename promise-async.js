feb05c5c8c53072b6081911e8d00341f69b7368c
Nikhil Jose <nikhiljose12345@gmail.com>
2022-12-12 14:18:37 +0530

    Intial commit

--- promise-async.js
+++ promise-async.js
@@ -1,21 +1,47 @@
 const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
 const newCookie = {name:"Biscotti Cookies"};
 
+console.log("promise-async.js")
+
 // Progression 1: create a function to get all the cookies
-
-   // Progression 2: using setTimeout() - use 1000 units for time parameter
-
+function getCookies(){
+    // Progression 2: using setTimeout() -- use 1000 units for time parameter
+    setTimeout(() => {
+        cookies.forEach((cookie) => {
+            $li = document.createElement("li");
+            $li.innerText = cookie.name;
+            document.body.append($li);
+        });
+    }, 1000);
+}
 
 //Progression 3: Create a function to creat cookies and create an object of Promise.
-
-
-    // Progression 4: adding two parameters and using setTimeout() - use 2000 units for time parameter
-
-      
-      // Progression 5: handling errors and adding new cookie to the list
-
+function createCookie(newCookie){
+    return new Promise((resolve,reject)=>{
+        // Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
+        setTimeout(() => {
+            // Progression 5: handling errors and adding new cookie to the list
+            if(!newCookie.name){
+                reject("Invalid Cookie!");
+            }
+            cookies.push(newCookie)
+            resolve();
+        }, 2000);
+    })
+}
 
 // Progression 7: creating a new async function
+async function pushAndDisplay(newCookie){
+   try{
+      await createCookie(newCookie);
+      getCookies();
+   }
+   catch(err){
+      console.log("Error", err);
+   }
+   
+
+}
 
 // calling the new async function
-
+pushAndDisplay(newCookie)
